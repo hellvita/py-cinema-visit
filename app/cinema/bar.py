03a06567a331e1bb7a9f1df7561bc276513e18fd
hellvita <seredaolga03@gmail.com>
2026-09-19 18:03:30 +0300

    Fix formatting

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+
 from app.people.customer import Customer
+
 
 class CinemaBar:
     @staticmethod
--- app/main.py
+++ app/main.py
@@ -4,8 +4,15 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    customer_instances = [Customer(name=c["name"], food=c["food"]) for c in customers]
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str,
+) -> None:
+    customer_instances = [
+        Customer(name=c["name"], food=c["food"]) for c in customers
+    ]
 
     for customer in customer_instances:
         CinemaBar.sell_product(
@@ -19,5 +26,5 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     hall.movie_session(
         movie_name=movie,
         customers=customer_instances,
-        cleaning_staff=cleaner_instance
+        cleaning_staff=cleaner_instance,
     )
